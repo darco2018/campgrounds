cf3ee26f93d6673fcb29cfe54c7fa308a8c97ea4
darco2018 <newsecond@outlook.com>
2019-09-23 19:36:53 +0200

    Display campgrounds

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,10 +3,21 @@ const request = require('request');
 
 const router = express.Router();
 
+function Campground(name, image) {
+  this.name = name;
+  this.image = image;
+}
+
+const inMemoryDb = [
+  new Campground('Camp1', 'https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732679d4954bcc5c_340.jpg'),
+  new Campground('Camp2', 'https://pixabay.com/get/5fe3dc46425ab108f5d084609620367d1c3ed9e04e50744f7c2d7bd69144c1_340.jpg'),
+  new Campground('Camp3', 'https://pixabay.com/get/57e8d7444251ae14f6da8c7dda793f7f1636dfe2564c704c732679d4954bcc5c_340.jpg'),
+];
+
 // /campgrounds
 router.get('/', (req, res) => {
-  res.send('Displaying list of campgrounds');
-  res.render('campgrounds');
+  // res.send('Displaying list of campgrounds');
+  res.render('campgrounds', { campgrounds: inMemoryDb });
 });
 
 router.post('/', (req, res) => {
