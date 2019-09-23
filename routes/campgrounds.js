1cb2530ec50cd547b2715f6ed7e03862dcbebbc1
darco2018 <newsecond@outlook.com>
2019-09-23 21:59:24 +0200

    Do views for  post/get in campgrounds.js

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -14,23 +14,24 @@ const inMemoryDb = [
   new Campground('Camp3', 'https://pixabay.com/get/57e8d7444251ae14f6da8c7dda793f7f1636dfe2564c704c732679d4954bcc5c_340.jpg'),
 ];
 
+router.get('/extra', (req, res) => {
+  res.render('extra');
+});
+
 // /campgrounds
 router.get('/', (req, res) => {
-  // res.send('Displaying list of campgrounds');
   res.render('campgrounds', { campgrounds: inMemoryDb });
 });
 
 // /campgrounds
 router.post('/', (req, res) => {
   console.log('Sending form data by POST');
-
   res.redirect('/campgrounds');
 });
 
 // /campgrounds/new
 router.get('/new', (req, res) => {
-  // res.send('Showing the form for a new campground');
-  res.render('newcampground');
+  res.render('new');
 });
 
 module.exports = router;
