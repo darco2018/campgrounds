222f241974bdaac8969433a51e4f717fffbda7fc
darco2018 <newsecond@outlook.com>
2019-09-25 17:54:06 +0200

    Connect to mongodb

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const request = require('request');
+const mongoose = require('mongoose');
 
 const router = express.Router();
 
-function Campground(name, image) {
+// ----------START create connection ----------
+const dbName = 'campgrounds';
+mongoose.connect(`mongodb://localhost:27017/${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+// ----------END create connection ----------
+
+db.once('open', () => {
+  console.log(`-------------------Successfully connected! to ${dbName}!--------------------`);
+});
+
+
+/* function Campground(name, image) {
   this.name = name;
   this.image = image;
-}
+} */
+
 
 const inMemoryDb = [
-  new Campground('Camp1', 'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
+  /* new Campground('Camp1', 'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'),
   new Campground('Camp2', 'https://pixabay.com/get/5fe3dc46425ab108f5d084609620367d1c3ed9e04e50744f7c2d7bd69144c1_340.jpg'),
   new Campground('Camp3', 'https://pixabay.com/get/57e8d7444251ae14f6da8c7dda793f7f1636dfe2564c704c732679d4954bcc5c_340.jpg'),
   new Campground('Camp4', 'https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732679d4954bcc5c_340.jpg'),
@@ -20,7 +33,7 @@ const inMemoryDb = [
   new Campground('Camp9', 'https://pixabay.com/get/57e8d7444251ae14f6da8c7dda793f7f1636dfe2564c704c732679d4954bcc5c_340.jpg'),
   new Campground('Camp10', 'https://pixabay.com/get/57e1d3404e53a514f6da8c7dda793f7f1636dfe2564c704c732679d4954bcc5c_340.jpg'),
   new Campground('Camp11', 'https://pixabay.com/get/5fe3dc46425ab108f5d084609620367d1c3ed9e04e50744f7c2d7bd69144c1_340.jpg'),
-  new Campground('Camp12', 'https://pixabay.com/get/57e8d7444251ae14f6da8c7dda793f7f1636dfe2564c704c732679d4954bcc5c_340.jpg'),
+  new Campground('Camp12', 'https://pixabay.com/get/57e8d7444251ae14f6da8c7dda793f7f1636dfe2564c704c732679d4954bcc5c_340.jpg'), */
 ];
 
 router.get('/extra', (req, res) => {
