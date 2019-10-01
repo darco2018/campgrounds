e67fc4afc5c0d00a98e97863b672fd801e763328
darco2018 <newsecond@outlook.com>
2019-10-01 19:03:46 +0200

    Implement adding a comment

--- models/comment.js
+++ models/comment.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const commentSchema = new mongoose.Schema({
   text: String,
-  author: String,
+  author: String
 });
 
 const commentModel = mongoose.model('Comment', commentSchema);
 
 module.exports = {
   commentSchema,
-  commentModel,
+  commentModel
   // method: function()
 };
