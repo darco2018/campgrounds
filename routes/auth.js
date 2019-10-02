6deaac9dd7c1008215c07ffd7a23b3c9be0cdd47
darco2018 <newsecond@outlook.com>
2019-10-02 12:55:51 +0200

    implement registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
+// authentication
+const passport = require('passport');
+//const LocalStrategy = require('passport-local');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const User = require('../models/user');
 
 // show register form
 router.get('/register', (req, res) => {
@@ -8,7 +14,23 @@ router.get('/register', (req, res) => {
 
 // process register data
 router.post('/register', (req, res) => {
-  res.send('REGISTER POST ROUTE');
+  const newUser = new User({
+    username: req.body.username
+  });
+  // passportLocalMongoose method to hash pswd
+  User.register(newUser, req.body.password, (err, user) => {
+    console.log('-------- Registering user ' + newUser.username);
+
+    if (err) {
+      console.log(err);
+      return res.render('auth/register');
+    }
+
+    passport.authenticate('local')(req, res, function() {
+      console.log('------------Local-authenticating');
+      res.redirect('/campgrounds');
+    });
+  });
 });
 
 module.exports = router;
