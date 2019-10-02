4ed2019903a1373d629739978ce12ed75d98fbef
darco2018 <newsecond@outlook.com>
2019-10-02 11:30:57 +0200

    Add password local mongoose to User schema

--- models/user.js
+++ models/user.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
+const passwordLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new mongoose.Schema({
   username: String,
   password: String
 });
+
+userSchema.plugin(passwordLocalMongoose);
 
 module.exports = mongoose.model('User', userSchema);
