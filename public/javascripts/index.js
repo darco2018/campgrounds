fdc619c09d5a5955672677c6a43c717f33fc46a0
darco2018 <newsecond@outlook.com>
2019-10-22 05:52:01 +0200

    Remove jQuery calls in index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,14 +1,5 @@
 $(document).ready(() => {
   const defaultImageUrl = '/images/default.jpg';
-
-  console.log($);
-  console.log(
-    'If you see this message without error before it, JQuery is working.'
-  );
-  console.log(
-    'To remove jQuery, remove the script link in view/partials/footer. Footer is part of landing.ejs'
-  );
-
   //------ Fix unloaded images with default image -----------
   const images = document.getElementsByTagName('img');
 
@@ -20,7 +11,6 @@ $(document).ready(() => {
     if (!isImgLoaded(image)) {
       image.setAttribute('src', defaultImageUrl);
     }
-    console.log(isLoaded);
   }
 
   // --------------------------------------------
