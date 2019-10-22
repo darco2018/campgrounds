aedfec2c7233c38eea24e07464e6a386170ad192
darco2018 <newsecond@outlook.com>
2019-10-22 10:06:47 +0200

    Add mock foodplaces in seeds

--- seeds.js
+++ seeds.js
@@ -1,117 +1,148 @@
 /* eslint-disable consistent-return */
 const mongoose = require('mongoose');
 const Dish = require('./models/dish');
-const Foodplace = require('./models/foodplace');
+const foodplace = require('./models/foodplace');
+const Foodplace = foodplace.foodplaceModel;
 const comment = require('./models/comment');
-
 const Comment = comment.commentModel;
 
-
-const mockComments = [
+const mockFoodplaces = [
   {
-    text:
-      'A great little park ( about 40 spaces ) which is very neat and well-kept. The staff is very friendly and each site was about 80 feet long. There is no cable. We dished at Gold Mountain RV Park in a Motorhome.',
-    author: 'Mike'
+    _id: '5d99f7e8b52e244adecf548b',
+    name: 'McRonald',
+    address: 'Czysta 5',
+    city: 'Kraków',
+    author: {
+      id: '5d999c195040be0ad32d5c38',
+      username: 'q'
+    },
+    description:
+      'Obsługa na dorbym poziomie. Potrawy bardzo dobre, prawdziwa rozkosz dla podniebienia. Atmosfera wnętrza zwielokrotnia przyjemne doznania.',
+    image:
+      'https://images.unsplash.com/photo-1556742393-d75f468bfcb0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    lat: 50.06399,
+    lng: 19.92692
   },
   {
-    text:
-      'This is our 3rd stay here. My 7/16 review gives a better description of the park. We again had a fairly level, FHU, pull through site that was satellite friendly. There is no cable or OTA TV. As others have noted, the Wi-Fi is very good. The lodge, bathrooms, and laundry are perfect for a small park like this one. While there is nothing recreational in the park, the reasonable nightly cost makes that negative acceptable.',
-    author: 'thomas@friendly.com'
+    _id: '5d99f8f0b52e244adecf548c',
+    name: "Harry's Bar",
+    address: 'Grodzka 8',
+    city: 'Kraków',
+    author: {
+      id: '5d999c195040be0ad32d5c38',
+      username: 'q'
+    },
+    description:'Wybraliśmy to miejsce, by świętować naszą rocznicę. Bardzo smaczne jedzenie, obsługa dyskretna i pomocna, a klimat warty wszytkiego!',
+    image:
+      'https://images.unsplash.com/photo-1556745750-68295fefafc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+    lat: 50.05973,
+    lng: 19.93785
   },
   {
-    text:
-      'Excellent WiFi. No cable or over air reception, but WiFi good enough to stream via our Roku. Manager very friendly as was dish host. We dished at Gold Mountain RV Park in a Fifth Wheel.',
-    author: 'Mike'
+    _id: '5d99f91fb52e244adecf548d',
+    name: 'U Chińczyka',
+    address: 'Szewska 12',
+    city: 'Kraków',
+    author: {
+      id: '5d999c195040be0ad32d5c38',
+      username: 'a'
+    },
+    description:
+      'Obsługa na dorbym poziomie. Potrawy bardzo dobre, prawdziwa rozkosz dla podniebienia. Atmosfera wnętrza zwielokrotnia przyjemne doznania.',
+    image:
+      'https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    lat: 50.06251,
+    lng: 19.93457
+  },
+  {
+    _id: '5d9a00f2b52e244adecf548e',
+    name: 'Pizza Cult',
+    address: 'Bronowicka 30',
+    city: 'Kraków',
+    author: {
+      id: '5d999c195040be0ad32d5c38',
+      username: 'a'
+    },
+    description: 'Zdecydowanie moja najbardziej ulubiona z tutejszych restauracji. Na uroczystą kolację w sam raz. Znakomita obsługa, jeszcze lepsze menu. Porcje niemałe, a potrawy bardzo dopracowane, na bardzo wysokim poziomie',
+    image:
+      'https://images.unsplash.com/photo-1555396273-367ea4eb4db5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+    lat: 50.0777912,
+    lng: 19.9005365
+  },
+  {
+    _id: '5d9a121e98605561e139e858',
+    name: 'Fish Club',
+    address: 'Dobrego Pasterza 24',
+    city: 'Kraków',
+    author: {
+      id: '5d999c195040be0ad32d5c38',
+      username: 'a'
+    },
+    description:'Minęły już 3 miesiące od naszej stypy, a ludzie wciąż je zachwalają i wspominają. My sami pamiętamy ten dzień z radością, co jest dużą zasługą Pana Wojtka, którzy świetnie nas ugościli i pomogli uczynić to przyjęcie niezapomnianym. Obsługa przemiła, jedzenie przepyszne ',
+    image:
+      'https://images.unsplash.com/photo-1525610553991-2bede1a236e2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    lat: 50.08766,
+    lng: 19.96083
+  },
+  {
+    _id: '5d9a35b6711f957454e97906',
+    name: 'Gruba Mysz',
+    address: 'Limanowskiego 20',
+    city: 'Kraków',
+    author: {
+      id: '5d999c195040be0ad32d5c38',
+      username: 'z'
+    },
+    description:'Cudne miejsce!!! Bajeczne jedzenie, muzyka na żywo i wybitne wina. Desery są prawdziwą ambrozją!!! Lokal ma ciekawą, ciepłą atmosferę.',
+    image:
+      'https://images.unsplash.com/photo-1514933651103-005eec06c04b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+    lat: 50.04491,
+    lng: 19.95338
+  },
+  {
+    _id: '5d9b6c0fa8959c67e3e41bcf',
+    name: 'Cuda u Marysi',
+    address: 'Wielicka 34',
+    city: 'Kraków',
+    author: {
+      id: '5d999c195040be0ad32d5c38',
+      username: 'z'
+    },
+    description: 'Wyraziste smaki. Małe, ale przytulne miejsce, obsługa na wysokim poziomie. Pełna kultura i życzliwość. Wrócę na pewno ',
+    image: 'https://images.unsplash.com/photo-1550966871-3ed3cdb5ed0c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+    lat: 50.03939,
+    lng: 19.96509
+  },
+  {
+    _id: '5d9ed875a3e2873531b7bbee',
+    name: 'Jama Smoka',
+    address: 'Królewska 22',
+    city: 'Kraków',
+    author: {
+      id: '5d999c195040be0ad32d5c38',
+      username: 'z'
+    },
+    description:'Niezobowiązująca atmosfera, idealna na spotkanie ze znajomymi (bywam tam w miarę możliwości), Do skosztowania polecam gulasz i grzyby – super!',
+    image: 'https://images.unsplash.com/photo-1551632436-cbf8dd35adfa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80',
+    lat: 50.07093,
+    lng: 19.92195
   }
 ];
 
-const mockDishes = [
-  {
-    name: 'Wooden Paradise',
-    image:
-      'https://images.unsplash.com/photo-1497900304864-273dfb3aae33?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    description:
-      'Our first class dishsites are unmatched in proximity to the lake and include water, electric (30/50 amp service), wireless internet and cable hook-ups, fire ring and picnic table. Whether you’re bringing a tent, travel trailer or a luxury motor home, you will find the perfect site.'
-  },
-  {
-    name: 'Starry Night',
-    image:
-      'https://images.unsplash.com/photo-1515408320194-59643816c5b2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    description:
-      'We don’t call this area the “middle of the park” for nothing - it’s centrally located to all of our amenities and is perfect for families with kids. With the best access to the pools, cafe, playground, and splash park, there are plenty of places to play and still be close to home. Guests looking for a little more quiet would be happier in the 700s, and satellite reception is best in the 900s and the odd-numbered 700s and 800s.'
-  },
-  {
-    name: 'Meadow Mountains',
-    image:
-      'https://images.unsplash.com/photo-1476041800959-2f6bb412c8ce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-    description:
-      'It is an area with older growth pine trees, making them some of the prettier and more wooded sites at Cherry Hill Park. Tucked away in their own loop, they offer more privacy while still being an easy walk to the bus depot and main office. Folks looking for a shady site will be happy here.'
-  }
-];
-
-async function seedDb(){
-
+async function seedDb() {
   try {
-    await Dish.remove({});
-    await Comment.remove({});
-    await Foodplace.remove({})
-    console.log("Removed dishes, comments, foodplaces.");
-  
-    for( const seed of mockDishes){
-      let savedDish = await Dish.create(seed);
-      console.log("Dish created");
-  
-      let savedComment = await Comment.create(
-        {
-          text: 'This place is great, but I wish there were more choices',
-          author: 'q'
-        }
-      )    
-      console.log("Comment created");
-      
-      savedDish.comments.push(savedComment);
-      savedDish.save();
-      console.log("Comment added to dish");    
+    await Dish.deleteMany({});
+    await Comment.deleteMany({});
+    await Foodplace.deleteMany({});
+    console.log('Removed dishes, comments, foodplaces.');
+
+    for (const seed of mockFoodplaces) {
+      let savedFoodplace = await Foodplace.create(seed);
+      console.log('Saved a foodplace');
     }
-    
   } catch (error) {
     console.log(error);
   }
-  
-}
-/* 
-
-PREVIOUS VERSION
-
-let commentsCounter = 0;
-function createDish(mockDish) {
-  //
-  Dish.create(mockDish, (err, savedDish) => {
-    if (err) {
-      console.log(`Error when saving dish: ${err}`);
-    }
-    console.log(`${savedDish.name} has been saved`);
-
-    // create comment
-    const newComment = mockComments[commentsCounter++];
-
-    Comment.create(newComment, (error, savedComment) => {
-      savedDish.comments.push(savedComment);
-
-      savedDish.save((er, dish) => {
-        if (er) console.log(`Error when saving dish: ${err}`);
-      });
-      console.log('Saved a comment');
-    });
-  });
 }
 
-function seedDb() {
-  Dish.remove({}, err => {
-    if (err) console.log(`Error when removing dishes: ${err}`);
-    console.log('Removed dishes');
-
-  });
-}
- */
 module.exports = seedDb;
