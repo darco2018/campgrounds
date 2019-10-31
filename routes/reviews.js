394c63f2b979d1ebb37062453bfd9ed063379729
darco2018 <newsecond@outlook.com>
2019-10-31 10:32:50 +0100

    Implement INDEX route

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,19 +4,22 @@ var Dish = require('../models/dish');
 var Review = require('../models/review');
 var middleware = require('../middleware');
 
-// Reviews Index
+// Reviews Index - display reviews for a given DISH
 router.get('/', function(req, res) {
-  res.render("review/index");
-  /* Dish.findById(req.params.id).populate({
+ 
+    //IMPORTANT! Replace id with req.params.id 
+  Dish.findById("5d999dbd4029930fa9e8ebd3").populate({
         path: "reviews",
         options: {sort: {createdAt: -1}} // sorting the populated reviews array to show the latest first
     }).exec(function (err, dish) {
+        
         if (err || !dish) {
             req.flash("error", err.message);
             return res.redirect("back");
-        }
+        }        
+        
         res.render("review/index", {dish: dish});
-    }); */
+    });
 });
 
 module.exports = router;
