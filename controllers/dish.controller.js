691c89ec4f7c89842d0c2c5b52d543f980d4f41a
darco2018 <newsecond@outlook.com>
2019-11-09 15:35:07 +0100

    Remove redundant cloudinery & multer

--- controllers/dish.controller.js
+++ controllers/dish.controller.js
@@ -16,21 +16,6 @@ const allowedDescriptionLength = 2000;
 
 /* ------------------------- IMAGE UPLOAD ------------------------------- */
 
-const multer = require('multer');
-const storage = multer.diskStorage({
-  filename: function(req, file, callback) {
-    callback(null, Date.now() + file.originalname);
-  }
-});
-const imageFilter = function(req, file, cb) {
-  // accept image files only
-  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
-    return cb(new Error('Only image files are allowed!'), false);
-  }
-  cb(null, true);
-};
-const upload = multer({ storage: storage, fileFilter: imageFilter });
-
 const cloudinary = require('cloudinary');
 cloudinary.config({
   cloud_name: 'darco',
--- routes/dishes.js
+++ routes/dishes.js
@@ -22,12 +22,6 @@ const imageFilter = function(req, file, cb) {
 };
 const upload = multer({ storage: storage, fileFilter: imageFilter });
 
-const cloudinary = require('cloudinary');
-cloudinary.config({
-  cloud_name: 'darco',
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-});
 
 /* ------------------------- ROUTES------------------------------- */
 
